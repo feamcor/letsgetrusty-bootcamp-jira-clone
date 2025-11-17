b60010976846c795f8f822a04710b16d4c8c92b2
Fábio Corrêa <feamcor@gmail.com>
2025-11-17 10:05:22 -0300

    Stage 2, Step 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod models;
 mod db;
 mod ui;
+mod io_utils;
 
 fn main() {
     println!("Welcome To Jira-CLI!");
---
+++ src/ui/prompts.rs
@@ -0,0 +1,70 @@
+use crate::{models::{Epic, Story, Status}, io_utils::get_user_input};
+
+pub struct Prompts {
+    pub create_epic: Box<dyn Fn() -> Epic>,
+    pub create_story: Box<dyn Fn() -> Story>,
+    pub delete_epic: Box<dyn Fn() -> bool>,
+    pub delete_story: Box<dyn Fn() -> bool>,
+    pub update_status: Box<dyn Fn() -> Option<Status>>
+}
+
+impl Prompts {
+    pub fn new() -> Self {
+        Self { 
+            create_epic: Box::new(create_epic_prompt),
+            create_story: Box::new(create_story_prompt),
+            delete_epic: Box::new(delete_epic_prompt),
+            delete_story: Box::new(delete_story_prompt),
+            update_status: Box::new(update_status_prompt)
+        }
+    }
+}
+
+fn create_epic_prompt() -> Epic {
+    println!("Epic Name:");
+    let name = get_user_input().trim().to_owned();
+    println!("Epic Description:");
+    let description = get_user_input().trim().to_owned();
+    Epic {
+        name,
+        description,
+        status: Status::Open,
+        stories: vec![]
+    }
+}
+
+fn create_story_prompt() -> Story {
+    println!("Story Name:");
+    let name = get_user_input().trim().to_owned();
+    println!("Story Description:");
+    let description = get_user_input().trim().to_owned();
+    Story {
+        name,
+        description,
+        status: Status::Open
+    }
+}
+
+fn delete_epic_prompt() -> bool {
+    println!("Are you sure you want to delete this epic? All stories in this epic will also be deleted [Y/n]:");
+    let yes_no = get_user_input().trim().to_lowercase();
+    yes_no == "y"
+}
+
+fn delete_story_prompt() -> bool {
+    println!("Are you sure you want to delete this story? [Y/n]:");
+    let yes_no = get_user_input().trim().to_lowercase();
+    yes_no == "y"
+}
+
+fn update_status_prompt() -> Option<Status> {
+    println!("New Status (1 - OPEN, 2 - IN-PROGRESS, 3 - RESOLVED, 4 - CLOSED");
+    let status = get_user_input().trim().parse::<u8>().unwrap();
+    match status {
+        1 => Some(Status::Open),
+        2 => Some(Status::InProgress),
+        3 => Some(Status::Resolved),
+        4 => Some(Status::Closed),
+        _ => None
+    }
+}
